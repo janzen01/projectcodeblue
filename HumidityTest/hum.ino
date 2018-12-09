1b9596cc62edd5274adcbf2136b755fa8c825d97
Ondra Hubalek <30392763+OndraHubalek@users.noreply.github.com>
2018-12-09 15:03:37 +0100

    Update hum.ino
    vlhkostComplete

--- HumidityTest/hum.ino
+++ HumidityTest/hum.ino
@@ -1,17 +1,32 @@
 #include <dht.h>
 
 dht DHT;
-
+ 
 #define DHT11_PIN 7
 
 void setup(){
+ 
   Serial.begin(115200);
-}
-
-loop()
-{
-    Serial.print("Vlhkost je ");
-    Serial.println(dht);
+  
+ 
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+ 
+    
+    int dht = DHT.read11(DHT11_PIN);
+    
+    Serial.print("Humidity = ");
+    Serial.print(DHT.humidity);
     Serial.println("%");
-delay(10000);
+
+
+
+
+    
+    delay(5000);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+ 
 }
